8735b9e893a6401e30e0f7978c5f1fbae31c0aa1
Andy01hb <128887067+Andy01hb@users.noreply.github.com>
2024-07-08 01:14:57 +0000

    estupido projecto por diversion y practica

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
+const insultos = [
 
+"Tonto",
+"Inútil",
+"Idiota",
+"Imbécil",
+"Necio",
+"Patán",
+"Bruto",
+"Bufón",
+"Torpe",
+"Bobalicón",
+"Zopenco",
+"Mentecato",
+"Chiflado",
+"Retardado",
+"Cenutrio",
+"Zángano",
+"Pelmazo",
+"Botarate",
+"Tarado",
+"Lerdo",
+
+];
+
+function generarinsulto() {
+    const randomindex = Math.floor(Math.random() * insultos.length);
+    const insulto = insultos[randomindex];
+    document.getElementById("insulto").innerText = insulto;
+}
+document.querySelector(".abutton").addEventListener("click", generarinsulto);
